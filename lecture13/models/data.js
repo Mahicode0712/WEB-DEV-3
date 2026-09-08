2c2ff8cc0275388e19b92b19d6fcf1ff6b79f350
Mahicode0712 <cloud.mahisingh@gmail.com>
2026-09-08 15:28:20 +0530

    fourth commit

--- lecture13/models/data.js
+++ lecture13/models/data.js
@@ -1,38 +1,44 @@
 let notes = [
-    {
-        id:1,
-        title:"Web Development",
-        description:"Web development is the work involved in developing a website for the Internet or an intranet. Web development can range from developing a simple single static page of plain text to complex web applications, electronic businesses, and social network services.",
-        note:"",
-        link:"",
-        author:"Aaron Hunter",
-        createdOn:"2024-06-10"
-    },
-    {
-        id:2,
-        title:"JavaScript",
-        description:"JavaScript is a programming language that conforms to the ECMAScript specification. JavaScript is high-level, often just-in-time compiled, and multi-paradigm.",
-        note:"",
-        link:"",
-        author:"Yujin Hart",
-        createdOn:"2023-06-11"
-    },
-    {
-        id:3,
-        title:"Node.js",
-        description:"Node.js is an open-source, cross-platform, back-end JavaScript runtime environment that runs on the V8 engine and executes JavaScript code outside a web browser.",
-        note:"",
-        link:"",    
-        author:"Euihyun lee",
-        createdOn:"2022-06-12"
-    },
-    {
-        id:4,
-        title:"Express.js",
-        description:"Express.js, or simply Express, is a back end web application framework for building RESTful APIs with Node.js. It is designed for building web applications and APIs.",
-        note:"",
-        link:"",
-        author:"Taejoo Kim",
-        createdOn:"2021-12-13"
-    }
-]
+  {
+    id: 1,
+    title: "Web Development",
+    description:
+      "Web development is the work involved in developing a website for the Internet or an intranet. Web development can range from developing a simple single static page of plain text to complex web applications, electronic businesses, and social network services.",
+    note: "",
+    link: "",
+    author: "Aaron Hunter",
+    createdOn: "2024-06-10",
+  },
+  {
+    id: 2,
+    title: "JavaScript",
+    description:
+      "JavaScript is a programming language that conforms to the ECMAScript specification. JavaScript is high-level, often just-in-time compiled, and multi-paradigm.",
+    note: "",
+    link: "",
+    author: "Yujin Hart",
+    createdOn: "2023-06-11",
+  },
+  {
+    id: 3,
+    title: "Node.js",
+    description:
+      "Node.js is an open-source, cross-platform, back-end JavaScript runtime environment that runs on the V8 engine and executes JavaScript code outside a web browser.",
+    note: "",
+    link: "",
+    author: "Euihyun lee",
+    createdOn: "2022-06-12",
+  },
+  {
+    id: 4,
+    title: "Express.js",
+    description:
+      "Express.js, or simply Express, is a back end web application framework for building RESTful APIs with Node.js. It is designed for building web applications and APIs.",
+    note: "",
+    link: "",
+    author: "Taejoo Kim",
+    createdOn: "2021-12-13",
+  },
+];
+
+module.exports = notes;
