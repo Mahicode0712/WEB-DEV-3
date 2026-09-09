74e8e969be9cd85460a0582b45365d3412da38fd
Mahicode0712 <cloud.mahisingh@gmail.com>
2026-09-09 11:40:28 +0530

    sixth commit

--- lecture13/controllers/notes_controller.js
+++ lecture13/controllers/notes_controller.js
@@ -1,7 +1,12 @@
-const notes = require('../models/data');
+const notes = require("../models/data");
 
 const getNotes = (req, res) => {
-    res.status(200).send(notes); 
-}
+  res.status(200).send(notes);
+};
+const getNoteById = (req, res) => {
+  let { id } = req.params;
+  let note = notes.find((note) => note.id === Number(id));
+  res.status(200).send(note);
+};
 
-module.exports = { getNotes };
+module.exports = { getNotes, getNoteById };
--- lecture13/routes/notesRoutes.js
+++ lecture13/routes/notesRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const { getNotes } = require("../controllers/node_controller");
+const { getNotes, getNoteById } = require("../controllers/notes_controller");
 
-router.get("/get-notes" ,getNotes);
+router.get("/get-notes", getNotes);
+router.get("/get-note/:id", getNoteById);
 module.exports = router;
