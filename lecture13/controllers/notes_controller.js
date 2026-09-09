2b69472916ce0108d058b100037a3562e401d3a0
Mahicode0712 <cloud.mahisingh@gmail.com>
2026-09-09 18:02:09 +0530

    seventh commit

--- lecture13/controllers/notes_controller.js
+++ lecture13/controllers/notes_controller.js
@@ -7,6 +7,10 @@ const getNoteById = (req, res) => {
   let { id } = req.params;
   let note = notes.find((note) => note.id === Number(id));
   res.status(200).send(note);
+  if (!note) {
+    res.status(404).send({ message: "Note not found" });
+  }
+  res.status(200).send(note);
 };
 
 module.exports = { getNotes, getNoteById };
